a1c7a8ab20f991207f6765f844536f2160338b0b
isha-singhal <isinghal40@gmail.com>
2022-07-02 03:02:47 +0530

    version 2.0

--- app.py
+++ app.py
@@ -1,18 +1,13 @@
-from flask import Flask, render_template, request,redirect,url_for
+from flask import Flask, render_template, request
 from matplotlib import ticker
 import yfinance as yf
 from package import fetch
 
 app = Flask(__name__)
 
-
-#sp500 = si.tickers_sp500()
-
-all_files = ['APPL','TSLA','AMZN']
 # The route() function of the Flask class is a decorator, which tells the application which URL should call the associated function.
 @app.route('/')
 def landing_function():
-    stock_files = list(all_files)
     return render_template('index.html')
 
 @app.route('/dashboard',methods=['GET','POST'])
